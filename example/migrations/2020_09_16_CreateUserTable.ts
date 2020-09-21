4a44eac0eee5bdeb95c233756571a4593c0c8a55
Stefano Ruth <stefano6262@gmail.com>
2020-09-21 08:45:16 +0200

    wip

--- example/migrations/2020_09_16_CreateUserTable.ts
+++ example/migrations/2020_09_16_CreateUserTable.ts
@@ -2,7 +2,7 @@ import { Schema } from '../../src'
 
 export default function () {
     return Schema.create('users', table => {
-        table.integer('id')
+        table.increments('id')
         table.string('email')
         table.string('password').nullable()
         table.string('name').nullable()
